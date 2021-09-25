d249a4b18bf6935fd1c4929b9eea6efa5c6aa6c5
Your Name <dangminhthanh032002@gmail.com>
2021-09-25 08:37:33 +0700

    giai ptrinh bac 2

--- src/Tuan3/tuan3/ChuongTrinhLab2Bai4.java
+++ src/Tuan3/tuan3/ChuongTrinhLab2Bai4.java
@@ -11,7 +11,7 @@ public class ChuongTrinhLab2Bai4 {
         System.out.println("2.Giải phương trình bậc 2");
         System.out.println("3.Tính tiền điện");
         int suLuacChon;
-        System.out.print("Lựa chọn chức năng");
+        System.out.print("Lựa chọn chức năng: ");
         Scanner bienNhap;
         bienNhap = new Scanner(System.in);
         suLuacChon = bienNhap.nextInt();
@@ -52,7 +52,28 @@ public class ChuongTrinhLab2Bai4 {
         }
     }
     static void giaiPTBac2() {
-        System.out.println("Trien khai code giải ptrinh bậc 2");
+        // ax2 + bx +c = 0
+        float a, b, c;
+        Scanner bienNhap = new Scanner(System.in);
+        do {
+            System.out.print("Nhap a khac 0: ");
+            a = bienNhap.nextFloat();
+        } while (a == 0);
+        System.out.print("Nhap b: ");
+        b = bienNhap.nextFloat();
+        System.out.print("Nhap c: ");
+        c = bienNhap.nextFloat();
+
+        float delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            System.out.println("PT vo nghiem!");
+        } else if (delta == 0) {
+            System.out.println("PT co nghiem kep: x1 = x2 = " + -b/ (2 * a));
+        } else {
+            double x1 = (-b+Math.sqrt(delta)) / (2*a);
+            double x2 = (-b-Math.sqrt(delta)) / (2*a);
+            System.out.println("PT co nghiem: x1=" + x1 + ", x2=" + x2);
+        }
     }
     static void tinhTienDien() {
         System.out.println("Triển khai code tính tiền điện");
