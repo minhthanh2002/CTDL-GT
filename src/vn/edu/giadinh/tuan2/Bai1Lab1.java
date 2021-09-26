0333d0fa76876bbbb71007a995f2fb399fc5a95a
Your Name <dangminhthanh032002@gmail.com>
2021-09-27 00:36:27 +0700

    bai 1 lab 1

--- src/vn/edu/giadinh/tuan2/Bai1Lab1.java
+++ src/vn/edu/giadinh/tuan2/Bai1Lab1.java
@@ -7,53 +7,15 @@ public class Bai1Lab1 {
     public static void main(String[] args) {
         System.out.println("Tinh chu vi hcn");
         System.out.println("Tinh dien tich hcn");
-        int suLuacChon;
-        System.out.print("Lựa chọn chức năng: ");
-        Scanner bienNhap;
-        bienNhap = new Scanner(System.in);
-        suLuacChon = bienNhap.nextInt();
-
-        switch (suLuacChon) {
-            case 1:
-                tinhChuVi();
-                break;
-            case 2:
-                tinhDienTich();
-                break;
-            default:
-                System.out.println("chon chuc nang khac");
-
-        }
-     bienNhap.close();
-    }
-
-     static void tinhChuVi() {
-         int dai, rong;
+        int dai, rong;
          Scanner bienNhap = new Scanner(System.in);
          System.out.println("Nhap chieu dai:");
          dai = bienNhap.nextInt();
          System.out.println("Nhap chieu rong:");
          rong = bienNhap.nextInt();
          System.out.println("Chu vi hcn la:" + (dai + rong) * 2);
-     }
+         System.out.println( "Dien tich hcn la:"  + dai * rong);
+         System.out.println("Canh nho nhat la :" + Math.min(dai, rong));
 
-     static void tinhDienTich() {
-        int dai, rong;
-        Scanner bienNhap = new Scanner(System.in);
-        System.out.println("Nhap chieu dai:");
-        dai = bienNhap.nextInt();
-        System.out.println("Nhap chieu rong:");
-        rong = bienNhap.nextInt();
-        System.out.println("Dien tich hcn la:" + dai * rong);
-        
-
-     }
-
-     
-
-         
- 
-         
-     
-    
+    }
 }
---
+++ src/vn/edu/giadinh/tuan2/Bai2Lab2.java
@@ -0,0 +1,14 @@
+package vn.edu.giadinh.tuan2;
+
+import java.util.Scanner;
+
+public class Bai2Lab2 {
+
+    public static void main(String[] args) {
+        System.out.println("Tinh the tich khoi hcn:");
+        
+    }
+
+
+    
+}
