15fa379f425d5838ead97c99a37ee4f40257e81b
Your Name <dangminhthanh032002@gmail.com>
2021-09-27 05:09:19 +0700

    Bai 2 Lab 3

---
+++ src/Tuan3/tuan3/Bai2Lab3.java
@@ -0,0 +1,13 @@
+package Tuan3.tuan3;
+
+public class Bai2Lab3 {
+    public static void main(String[] args) {
+        for (int i = 2;  i<= 9; i++){
+        for (int j=1; j<=10; j++){
+            System.out.println(i + " x " + j + " = "  + i*j);
+        }
+    }
+
+}
+    
+}
