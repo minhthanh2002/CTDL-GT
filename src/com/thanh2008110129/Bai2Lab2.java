1bb720650dcbca33b8ab8903844075c48fe5ebcd
Your Name <dangminhthanh032002@gmail.com>
2021-09-27 21:18:14 +0700

    Bai tap

---
+++ src/com/thanh2008110129/Bai2Lab2.java
@@ -0,0 +1,47 @@
+package com.thanh2008110129;
+
+import java.util.Scanner;
+
+public class Bai2Lab2 {
+    public static void main(String[] args) {
+        System.out.println("Giai ptrinh bac hai:");
+        float a,b,c;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        a = sc.nextFloat();
+        System.out.println("Nhap b:");
+        b = sc.nextFloat();
+        System.out.println("Nhap c:");
+        c = sc.nextFloat();
+
+        if (a==0){
+            //giai ptrinh bac 1 
+        } if (b==0){
+            if(c==0){
+                System.out.println("Phuong trinh vo so nghiem");
+            }else{
+                System.out.println("Phuong trinh vo nghiem:");
+            }
+        
+        }else{
+                System.out.println("Phuong trinh co nghiem x=" + -b/c);
+            }
+            //phuong trinh bac 2
+            float delta = b * b - 4 * a * c;
+        if (delta < 0) {
+            System.out.println("PT vo nghiem!");
+        } else if (delta == 0) {
+            System.out.println("PT co nghiem kep: x1 = x2 = " + -b/ (2 * a));
+        } else {
+            double x1 = (-b+Math.sqrt(delta)) / (2*a);
+            double x2 = (-b-Math.sqrt(delta)) / (2*a);
+            System.out.println("PT co nghiem: x1=" + x1 + ", x2=" + x2);
+        }
+    }
+}       
+         
+         
+         
+
+    
+
--- src/com/thanh2008110129/Bai2Lab3.java
+++ src/com/thanh2008110129/Bai2Lab3.java
@@ -11,3 +11,6 @@ public class Bai2Lab3 {
 }
     
 }
+
+    
+
