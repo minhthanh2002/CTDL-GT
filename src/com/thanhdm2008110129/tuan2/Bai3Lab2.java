2c485c54a5af9b710ba109c5f24fb3c3a890034e
Your Name <dangminhthanh032002@gmail.com>
2021-09-28 19:32:56 +0700

    day bai

---
+++ src/com/thanhdm2008110129/tuan2/Bai3Lab2.java
@@ -0,0 +1,20 @@
+package com.thanhdm2008110129.tuan2;
+
+import java.util.Scanner;
+
+public class Bai3Lab2 {
+    public static void main(String[] args) {
+        System.out.println("Nhap so dien tieu thu:");
+        float soDien;
+        Scanner sc = new Scanner(System.in);
+        soDien = sc.nextFloat();
+
+        if (soDien <=50){
+            System.out.println("So tien phai tra:" + soDien * 1000);
+        }else if (soDien > 50){
+            System.out.println( "So tien phai tra:" + 50 * 1000 + (soDien- 50) * 1200);
+
+        }
+    }
+}
+
---
+++ src/com/thanhdm2008110129/tuan2/Bai4Lab1.java
@@ -0,0 +1,24 @@
+package com.thanhdm2008110129.tuan2;
+
+import java.util.Scanner;
+
+public class Bai4Lab1 {
+    public static void main(String[] args) {
+        System.out.println("Tinh delta");
+        System.out.println("Tinh can bac 2");
+        float a,b,c;
+        Scanner bienNhap = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        a = bienNhap.nextFloat();
+        System.out.println("Nhap b:");
+        b = bienNhap.nextFloat();
+        System.out.println("Nhap c:");
+        c = bienNhap.nextFloat();
+        float delta = (b*b) - 4*a*c;
+        System.out.println("Can bac 2 cua delta =" +  Math.sqrt(delta) );
+        
+        
+    }
+
+    
+}
