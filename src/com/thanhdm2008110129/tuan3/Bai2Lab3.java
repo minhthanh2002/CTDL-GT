f58533908e88f64356b4a938cdd7d47a31cb65d2
Your Name <dangminhthanh032002@gmail.com>
2021-09-28 19:47:51 +0700

    sua bai

---
+++ src/com/thanhdm2008110129/tuan3/Bai2Lab3.java
@@ -0,0 +1,12 @@
+package com.thanhdm2008110129.tuan3;
+
+public class Bai2Lab3 {
+    public static void main(String[] args) {
+        for(int i=2; i <= 9; i++){
+            for(int j=1; j <= 10; j++){
+                System.out.println(i + "x" + j + "=" + i*j);
+            }
+        }
+    }
+    
+}
