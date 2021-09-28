84f3509f1ddc90c0cfe37f462d6555009958cb47
Your Name <dangminhthanh032002@gmail.com>
2021-09-28 19:02:31 +0700

    Bai tap

---
+++ src/com/thanh2008110129/tuan2/Bai1Lab2.java
@@ -0,0 +1,31 @@
+package com.thanh2008110129.tuan2;
+
+import java.util.Scanner;
+
+public class Bai1Lab2 {
+    public static void main(String[] args) {
+        System.out.println("Giai ptrinh bac nhat:");
+        float a,b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        a = sc.nextFloat();
+        System.out.println("Nhap b:");
+        b = sc.nextFloat();
+
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("PT co vo so nghiem! ");
+            } else {
+                System.out.println("PT vo nghiem");
+            }
+        } else {
+            System.out.println("PT co nghiem: " + (-b/a));
+        }
+    }
+         
+
+         
+
+    
+    
+}
