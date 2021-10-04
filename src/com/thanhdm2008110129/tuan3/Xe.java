ccc486696f7a993b4fd7eb2836b86b519001f119
Your Name <dangminhthanh032002@gmail.com>
2021-10-04 19:04:58 +0700

    bai tap

---
+++ src/com/thanhdm2008110129/tuan3/Xe.java
@@ -0,0 +1,24 @@
+package com.thanhdm2008110129.tuan3;
+
+public class Xe {
+    String tenChuXe;
+    String hangSanXuat;
+    String dong; 
+    String dungTichXang;
+
+    void inThongTin(){
+        System.out.println("Ten chu xe: " + tenChuXe);
+        System.out.println("Hang san xuat " + hangSanXuat;
+        System.out.println("Dong: "+ dong );
+        System.out.println("Dung tich xang" + dungTichXang);
+
+    }
+    Xe(String t, String h, String d, String x){
+        System.out.println();
+    }
+
+    
+
+
+    
+}
