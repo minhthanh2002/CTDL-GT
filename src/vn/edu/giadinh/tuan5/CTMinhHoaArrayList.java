42d5ed3f869f54364888be7c5640482af9bed6a0
Your Name <dangminhthanh032002@gmail.com>
2021-10-11 18:44:21 +0700

    tuan 5

---
+++ src/vn/edu/giadinh/tuan5/CTMinhHoaArrayList.java
@@ -0,0 +1,22 @@
+package vn.edu.giadinh.tuan5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CTMinhHoaArrayList {
+    public static void main(String[] args) {
+        ArrayList a = new ArrayList();
+        a.add("Cuong");
+        a.add(true);
+        a.add(33);
+        a.add(2002);
+        Integer x = (Integer)a.get(2);
+        
+        System.out.println(a);
+
+        List bienArraList = new ArrayList();
+
+
+    }
+    
+}
